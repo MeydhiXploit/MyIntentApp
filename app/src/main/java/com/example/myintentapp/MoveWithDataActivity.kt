4a5dbb397a3f3934cb5068457d5cb0e55b7e9397
MeydhiXploit <meydhimeydhi@gmail.com>
2024-09-25 18:22:43 +0800

    feat : move with data

--- app/src/main/java/com/example/myintentapp/MoveWithDataActivity.kt
+++ app/src/main/java/com/example/myintentapp/MoveWithDataActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myintentapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,6 @@ class MoveWithDataActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val tvDataReceived:TextView = findViewById(R.id.tv_data_received)
     }
 }
